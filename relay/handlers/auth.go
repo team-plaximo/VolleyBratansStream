516c14b77093d3cfcf8a5ff8b140d604e3267b6e
team-plaximo <122498458+team-plaximo@users.noreply.github.com>
2026-01-23 18:20:59 +0100

    fix: remove unused log imports from handlers and middleware

--- relay/handlers/auth.go
+++ relay/handlers/auth.go
@@ -2,7 +2,6 @@ package handlers
 
 import (
 	"encoding/json"
-	"log"
 	"net/http"
 	"time"
 	"github.com/volleybratans/moblin-relay/services"
--- relay/middleware/auth.go
+++ relay/middleware/auth.go
@@ -2,7 +2,6 @@ package middleware
 
 import (
 	"encoding/json"
-	"log"
 	"net/http"
 	"time"
 	"github.com/volleybratans/moblin-relay/services"
